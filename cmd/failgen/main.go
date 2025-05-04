713331d3826df659be8f5de53d97fc81c9137c0d
Gaetan Wcz <gaetan.wichlacz@ext.adeo.com>
2025-05-04 14:58:17 +0200

    feat(health): add monitoring endpoint

--- cmd/failgen/main.go
+++ cmd/failgen/main.go
@@ -9,6 +9,10 @@ import (
 
 func main() {
 	http.HandleFunc("/fail", handler.FailHandler)
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
 	log.Println("[FailGen] Listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
